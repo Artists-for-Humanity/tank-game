883548e9ca3965526f92c5984b4fc755f256d093
ZAE42 <tylerhuupham@gmail.com>
2026-08-06 17:17:36 -0400

    heli copta

--- Assets/Scripts/AI/HelicopterAI.cs
+++ Assets/Scripts/AI/HelicopterAI.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class HelicopterAI : MonoBehaviour
 {
+    private NavMeshAgent agent;
+    public GameObject follow;
+    public Rigidbody rigidBody;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
